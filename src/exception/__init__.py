9b0af178a0c472e8feac731147ce357c1083a580
Prem Mote <moteprem4@gmail.com>
2026-05-20 22:20:17 +0530

    added new edited files

--- src/exception/__init__.py
+++ src/exception/__init__.py
@@ -46,3 +46,4 @@ class MyException(Exception):
         Returns the string representation of the error message.
         """
         return self.error_message
+    
--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler
+from from_root import from_root
 from datetime import datetime
 
 # Constants for log configuration
@@ -9,11 +10,8 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 BACKUP_COUNT = 3  # Number of backup log files to keep
 
-# Project root directory (two levels up from this file)
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-
 # Construct log file path
-log_dir_path = os.path.join(ROOT_DIR, LOG_DIR)
+log_dir_path = os.path.join(from_root(), LOG_DIR)
 os.makedirs(log_dir_path, exist_ok=True)
 log_file_path = os.path.join(log_dir_path, LOG_FILE)
 
